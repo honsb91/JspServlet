7a17af47f5544ab055a7efbd96650e0bf18ec1a2
honsb91 <114128759+honsb91@users.noreply.github.com>
2023-11-16 17:49:22 +0900

    .
    .

---
+++ 03.Implicit/src/main/java/controller/UserTblController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import usertbl.UserTblDAO;
+import usertbl.UserTblDTO;
+
+// name , urlpatterns.... 추천 x 
+// *.user , *.do .. 주의 : /를 빼야함.
+@WebServlet(name = "usertbl", urlPatterns = { "/list", "/detail" , "/update" })
+public class UserTblController extends HttpServlet {
+	// url요청이 list이면 전체목록을 list.jsp가 보여주게함.
+	// url요청이 detail이면 회원 한명의 정보를 detail.jsp가 보여주게함.
+	// 요청에 따라 콘솔창에 list , detail이 찍히게 처리해보기.
+	RequestDispatcher rd;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		
+		UserTblDAO dao = new UserTblDAO();
+		if (req.getServletPath().equals("/list")) {
+			ArrayList<UserTblDTO> list = dao.selectList();
+			req.setAttribute("list", list);
+			rd = req.getRequestDispatcher("usertbl/list.jsp");
+		} else if (req.getServletPath().equals("/detail")) {
+			String name = req.getParameter("name");
+			UserTblDTO dto = dao.selectOne(name);
+			req.setAttribute("dto", dto);
+			rd = req.getRequestDispatcher("usertbl/detail.jsp");
+		}else if (req.getServletPath().equals("/update")) {
+			UserTblDTO dto = new UserTblDTO();
+			dto.setUsername(req.getParameter("username"));
+			dto.setBirthyear(Integer.parseInt(req.getParameter("birthyear")));
+			dto.setMobile(req.getParameter("mobile"));
+			dto.setAddress(req.getParameter("address"));
+			
+		}
+
+		rd.forward(req, resp);
+	}
+}
--- 03.Implicit/src/main/webapp/index.jsp
+++ 03.Implicit/src/main/webapp/index.jsp
@@ -303,23 +303,13 @@
 		<div class="container px-5 my-5">
 			<div class="text-center mb-5">
 				<h1 class="display-5 fw-bolder mb-0">
-					<span class="text-gradient d-inline">스크립트태그(Script Tag)</span>
+					<span class="text-gradient d-inline">Request(요청)</span>
 				</h1>
 			</div>
 			<div class="row gx-5 justify-content-center">
 				<div class="col-lg-11 col-xl-9 col-xxl-8">
 					<!-- Experience Section-->
 					<section>
-						<div
-							class="d-flex align-items-center justify-content-between mb-4">
-							<h2 class="text-primary fw-bolder mb-0">Request(요청)</h2>
-							<!-- Download resume button-->
-							<!-- Note: Set the link href target to a PDF file within your project-->
-							<a class="btn btn-primary px-4 py-3" href="#!">
-								<div class="d-inline-block bi bi-download me-2">Download
-									Resume</div>
-							</a>
-						</div>
 						<!-- Experience Card 1-->
 						<div class="card shadow border-0 rounded-4 mb-4">
 							<div class="card-body p-3">
